d0ec67990e5a5ee4830137b7c40e4d5f4e583396
nu99etz <dwiciptan99@gmail.com>
2023-12-08 23:29:09 +0700

    [crud] master > siswa

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->post('/student', 'Service\Master\StudentController::getStudent');
+$routes->post('/student/add','Service\Master\StudentController::addStudent');
+$routes->post('/student/delete','Service\Master\StudentController::deleteStudent');
