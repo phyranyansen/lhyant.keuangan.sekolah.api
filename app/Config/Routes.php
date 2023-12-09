0a56d69e3332c76c8acd360f3ee487a3e40ece70
phyranyansen <phyranyansen>
2023-12-09 09:42:59 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->post('login', 'Login::index');
+$routes->post('token/refresh', 'Token::refreshToken');
+
+$routes->get('/', 'Home::index');
+
+$routes->post('/student', 'Service\Master\StudentController::getStudent');
+$routes->post('/student/add','Service\Master\StudentController::addStudent');
+$routes->post('/student/delete','Service\Master\StudentController::deleteStudent');
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('me', 'Me::index', ['filter' => 'auth']);
 $routes->get('users', 'User::index', ['filter' => 'auth']);
--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -18,6 +18,8 @@ class Login extends ResourceController
     public function __construct()
     {
         $this->user = new UserModel();
+        //controller -------------------
+        $this->token = new Token();
     }
 
     public function index()
@@ -38,80 +40,10 @@ class Login extends ResourceController
 
         if (!sha1($password, $passwordHash)) return $this->fail('Password salah!');
        
-        $jwt = $this->generateToken($user);
+        $jwt = $this->token->index($user);
 
         return $this->respond($jwt);
     }
 
 
-
-    public function refreshToken()
-    {
-        $key = getenv('TOKEN_SECRET');
-        $header = $this->request->getServer('HTTP_AUTHORIZATION');
-        if (!$header) return $this->failUnauthorized('Token Authorization');
-       
-    
-        $token = explode(' ', $header)[1];
-        try {
-           
-        $decoded = JWT::decode($token, new Key($key, 'HS256'));
-              
-            //Cek token apakah masih valid atau sudah tidak valid lagi
-          
-            if (time() <= strtotime($decoded->tokenExpTime)) {
-                $user = $this->user->find($decoded->uid);
-                if (!$user) return $this->failNotFound('User not found');
-                
-                $jwt = $this->generateToken($user);
-
-                return $this->respond($jwt);
-            } else {
-                $debug = [
-                    'now'   => time(),
-                    'expTime' => strtotime($decoded->tokenExpTime)
-                ];
-                print_r($debug);
-                return $this->failUnauthorized('Token is still valid');
-            }
-            
-        } catch (\Throwable $th) {
-            return $this->fail('Invalid Token!');
-        }
-      
-    }
-
-    protected function generateToken($user)
-    {
-        $key = getenv('TOKEN_SECRET');
-    
-        // Waktu expired 1 jam
-        $expiration = time() +  3600;
-        $expirationDateTime = new \DateTime('@' . $expiration);
-        $expirationDateTime->setTimezone(new \DateTimeZone('Asia/Jakarta'));
-        $expirationString = $expirationDateTime->format('Y-m-d\TH:i:s.u\Z');
-    
-        //random code for refresh
-        $refreshToken = bin2hex(random_bytes(32));
-
-        $payload = [
-            'tokenExpTime'   => $expirationString,
-            'refreshToken'   => $refreshToken, 
-            'uid'            => $user['user_id'],
-            'email'          => $user['user_email']
-        ];
-    
-        $jwt = JWT::encode($payload, $key, 'HS256');
-        $response = [
-            'token'          => $jwt,
-            'tokenExpTime'   => $expirationString,
-            'refreshToken'   => $refreshToken, 
-            'user_id'        => $user['user_id']
-        ];
-
-        session()->set($response);
-    
-        return $response;
-    }
-    
 }
