945dd54c0a1f79959a779f1ff4a84e6e2218d739
phyranyansen <phyranyansen>
2023-12-09 09:40:57 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->post('login', 'Login::index');
+$routes->post('token/refresh', 'Token::refreshToken');
+
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
-$routes->post('token/login', 'Login::index');
-$routes->post('token/refresh', 'Login::refreshToken');
 $routes->get('me', 'Me::index', ['filter' => 'auth']);
 $routes->get('users', 'User::index', ['filter' => 'auth']);
