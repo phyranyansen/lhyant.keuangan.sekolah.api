546c3aaee753af367a11e4dddb635cb132672aab
phyranyansen <phyranyansen>
2023-12-09 09:44:56 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,9 +10,9 @@ $routes->post('token/refresh', 'Token::refreshToken');
 
 $routes->get('/', 'Home::index');
 
-$routes->post('/student', 'Service\Master\StudentController::getStudent');
-$routes->post('/student/add','Service\Master\StudentController::addStudent');
-$routes->post('/student/delete','Service\Master\StudentController::deleteStudent');
+$routes->post('/student', 'Service\Master\StudentController::getStudent', ['filter' => 'auth']);
+$routes->post('/student/add','Service\Master\StudentController::addStudent', ['filter' => 'auth']);
+$routes->post('/student/delete','Service\Master\StudentController::deleteStudent', ['filter' => 'auth']);
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('me', 'Me::index', ['filter' => 'auth']);
 $routes->get('users', 'User::index', ['filter' => 'auth']);
